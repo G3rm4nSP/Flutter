9029f224e340ce7a6881bc89862e6620581d7687
GermánSánchezPortill <gsanchezp@eepmad.com>
2023-03-29 01:54:46 +0200

    stateful de los productos y carrito

--- flutter_nevera/lib/home_screen/home.dart
+++ flutter_nevera/lib/home_screen/home.dart
@@ -1,13 +1,27 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 import 'cards_food.dart';
-import 'cart_product.dart';
+import 'app_bar.dart';
+import 'shoping_cart_scaffold/cart_product.dart';
+import 'shoping_cart_scaffold/shoping_cart.dart';
+
 import 'drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _carrito = false;
+
+  void _mostrarCarrito(bool newValue) {
+    setState(() {
+      _carrito = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,159 +36,42 @@ class HomeScreen extends StatelessWidget {
           colorScheme: colorScheme),
     );
 
-    final List<CartProduct> cart = List<CartProduct>.generate(
-      15,
-      (i) => CartProduct(
-          nombre: 'Item $i',
-          cantidad: i,
-          precio: i * 10,
-          image: AssetImage('assets/${(i + 1) % 10}.jpg'),
-          seleccionadas: i,
-          colorScheme: colorScheme),
-    );
+    //lista incrementable CartProduct
 
     return SafeArea(
       child: Scaffold(
         backgroundColor: colorScheme.primary,
-        appBar: AppBar(
-          backgroundColor: colorScheme.inversePrimary,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              SizedBox(
-                width: 5.w,
-              ),
-              const Text("LA NEVERA"),
-              IconButton(
-                  onPressed: () {
-                    print("Carrito");
-                  },
-                  tooltip: "Carrito",
-                  icon: const Icon(Icons.shopping_cart))
-            ],
-          ),
-          leading: Builder(
-            builder: (BuildContext context) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Ink(
-                  decoration: ShapeDecoration(
-                    color: colorScheme.inversePrimary,
-                    shape: CircleBorder(
-                        side: BorderSide(color: colorScheme.onPrimary)),
-                  ),
-                  child: const DecoratedBox(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: AssetImage("assets/avatar.jpg")),
-                    ),
-                  ),
-                ),
-              );
-            },
-          ),
+        //APPBAR
+        appBar: PreferredSize(
+          preferredSize: Size(1.w, 7.h),
+          child: HomeAppBar(
+              colorScheme: colorScheme,
+              mostrarCarrito: _mostrarCarrito,
+              active: _carrito),
         ),
+
         drawer: PrincipalDrawer(colorScheme: colorScheme),
+        //BODY
         body: Stack(
           children: [
             ListView.builder(
-              itemCount: cart.length,
-              itemBuilder: (context, index) {
-                return cart[index];
+              itemCount: cards.length,
+              itemBuilder: (
+                context,
+                index,
+              ) {
+                return cards[index];
               },
             ),
-            /*FloattingBoxCart(
-              colorScheme: colorScheme,
-              total: 20,
-              cart: cart,
-            )*/
+            SizedBox(
+              height: _carrito ? 100.h : 0,
+              child: ShoppingCart(
+                colorScheme: colorScheme,
+                totalCesta: 20,
+              ),
+            ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class FloattingBoxCart extends StatelessWidget {
-  const FloattingBoxCart({
-    super.key,
-    required this.colorScheme,
-    required this.total,
-    required this.cart,
-  });
-
-  final List<CartProduct> cart;
-  final double total;
-  final ColorScheme colorScheme;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: colorScheme.onPrimary.withOpacity(0.8),
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          const Text(
-            "Carrito",
-            style: TextStyle(fontSize: 20),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-            thickness: 1,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ListView.builder(
-            itemCount: cart.length,
-            itemBuilder: (context, index) {
-              return cart[index];
-            },
-          ),
-          const Text(
-            "No hay productos en el carrito",
-            style: TextStyle(fontSize: 20),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-            thickness: 1,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            'Total: $total €',
-            style: const TextStyle(fontSize: 20),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.black,
-            thickness: 1,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          ElevatedButton(
-            onPressed: () {
-              print("Comprar");
-            },
-            child: const Text("Comprar"),
-          ),
-        ],
       ),
     );
   }
---
+++ flutter_nevera/lib/home_screen/shoping_cart_scaffold/cart_product.dart
@@ -0,0 +1,154 @@
+//CARTAS
+import 'package:flutter/material.dart';
+
+class CartProduct extends StatelessWidget {
+  const CartProduct({
+    required this.nombre,
+    required this.cantidad,
+    required this.precio,
+    required this.image,
+    required this.total,
+    this.seleccionadas = 0,
+    super.key,
+    required this.colorScheme,
+  });
+
+  final double total;
+  final ColorScheme colorScheme;
+  final String nombre;
+  final int cantidad;
+  final double precio;
+  final AssetImage image;
+  final int seleccionadas;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Divider(
+          color: Colors.grey,
+          thickness: 1,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Informacion(
+                nombre: nombre,
+                seleccionadas: seleccionadas,
+                precio: precio,
+                total: total,
+                colorScheme: colorScheme),
+            Column(
+              children: [
+                Imagen(image: image),
+                Row(
+                  children: [
+                    ButtonCart(colorScheme: colorScheme, action: true),
+                    Text('  $cantidad  '),
+                    ButtonCart(colorScheme: colorScheme, action: false),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class Imagen extends StatelessWidget {
+  const Imagen({
+    super.key,
+    required this.image,
+  });
+
+  final AssetImage image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(right: 10),
+      height: 50,
+      width: 60,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          image: DecorationImage(image: image),
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonCart extends StatelessWidget {
+  const ButtonCart({
+    super.key,
+    required this.colorScheme,
+    required this.action,
+  });
+
+  final bool action;
+  final ColorScheme colorScheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      style: ButtonStyle(
+        iconSize: MaterialStateProperty.all(20),
+        fixedSize: MaterialStateProperty.all(const Size(40, 40)),
+        backgroundColor: MaterialStateProperty.all(colorScheme.inversePrimary),
+        shape: MaterialStateProperty.all(
+          CircleBorder(
+            side: BorderSide(color: colorScheme.onPrimary),
+          ),
+        ),
+      ),
+      icon: (action ? const Icon(Icons.add) : const Icon(Icons.remove)),
+      color: colorScheme.onPrimary,
+      onPressed: () {
+        //Navigator.pop(context);
+      },
+    );
+  }
+}
+
+class Informacion extends StatelessWidget {
+  const Informacion({
+    super.key,
+    required this.nombre,
+    required this.seleccionadas,
+    required this.precio,
+    required this.total,
+    required this.colorScheme,
+  });
+
+  final ColorScheme colorScheme;
+  final String nombre;
+  final int seleccionadas;
+  final double precio;
+  final double total;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          nombre,
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: colorScheme.onPrimaryContainer),
+        ),
+        const SizedBox(height: 15),
+        Text(
+          'Total $total €',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: colorScheme.onPrimaryContainer),
+        ),
+      ],
+    );
+  }
+}
